82c0eed448c22025ed92c5a1344e34a1cf0e1f2a
Luiz Victor Alvarenga Andrade <victorzld.andrade@gmail.com>
2025-10-02 12:04:25 -0300

    chore(db): Add migration for test data
    - Create V9__insertCoursesAndRegistrations.sql to insert sample courses and registrations.
    
    - This data is used to manually test the registration report functionality and ensure the ordering is correct.

---
+++ alura-case-main/src/main/resources/db/migration/V9__insertCoursesAndRegistrations.sql
@@ -0,0 +1,12 @@
+INSERT INTO Course (name, code, instructor_id, category_id, status) VALUES
+('Java Basico', 'java-basic', 1, 1, 'ACTIVE'),
+('Spring Boot', 'spring-bt', 1, 1, 'ACTIVE'),
+('HTML e CSS', 'html-css', 1, 2, 'ACTIVE');
+
+INSERT INTO Registration (user_id, course_id, registration_date) VALUES (2, 1, NOW());
+INSERT INTO Registration (user_id, course_id, registration_date) VALUES (3, 1, NOW());
+INSERT INTO Registration (user_id, course_id, registration_date) VALUES (4, 1, NOW());
+INSERT INTO Registration (user_id, course_id, registration_date) VALUES (5, 1, NOW());
+INSERT INTO Registration (user_id, course_id, registration_date) VALUES (2, 2, NOW());
+INSERT INTO Registration (user_id, course_id, registration_date) VALUES (3, 2, NOW());
+INSERT INTO Registration (user_id, course_id, registration_date) VALUES (4, 3, NOW());
